53d4dce98e51ebd92aeade880048195878841af9
calebkabaya <calebkabaya@gmail.com>
2022-04-13 16:45:21 +0300

    Auto-generated commit [ci skip]

--- src/app/mealList.ts
+++
@@ -1,8 +0,0 @@
-import { Meals } from "./meals";
-
-
-export let MealsList:Meals[] = [
-    new Meals(1,'chicken',270,'For Lunch'),
-    new Meals(2,'Beef stew',500,'For Supper'),
-    new Meals(3,'French toast with Eggs',400,'For BreakFast'),
-]
